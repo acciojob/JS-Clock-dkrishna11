3d4bcaa4c422b2dc38cbf14559ce052e1aaffa06
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2024-01-22 18:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,20 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function updateClock() {
     // Get current time
-    const now = new Date();
-    const hours = now.getHours() % 12; // Convert to 12-hour format
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+     var date = new Date();
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hours = date.getHours();
 
-    // Calculate rotation angles for clock hands
-    const hourRotation = (360 / 12) * (hours + minutes / 60);
-    const minRotation = (360 / 60) * (minutes + seconds / 60);
-    const secondRotation = (360 / 60) * seconds;
+    let hrPosition = hours*360/12 + ((minutes * 360/60)/12) ;
+    let minPosition = (minutes * 360/60) + (seconds* 360/60)/60;
+    let secPosition = seconds * 360/60;
 
-    // Apply rotation to clock hands
-    hourHand.style.transform = `rotate(${hourRotation}deg)`;
-    minHand.style.transform = `rotate(${minRotation}deg)`;
-    secondHand.style.transform = `rotate(${secondRotation}deg)`;
+    hourHand.style.transform = 'rotate(' + hrPosition + 'deg)';
+    minHand.style.transform = 'rotate(' + minPosition + 'deg)';
+    secondHand.style.transform = 'rotate(' + secPosition + 'deg)';
   }
 
   // Update clock every second
